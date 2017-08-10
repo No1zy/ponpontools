78f706ffa2acb4cd4d4d700d7e3ff39e11fc875a
No1zy <bgkn1260128@gn.iwasaki.ac.jp>
2017-08-10 12:04:06 -0700

    exploit.py: updated

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+echo "alias pingg='ping -c 1 google.co.jp'" >> $HOME/.bashrc
+echo "alias curlg='curl google.co.jp'" >> $HOME/.bashrc
+
 echo 'export PATH=$PATH:$HOME/ponpontools/commands/' >> $HOME/.bashrc
-echo -e "\e[33mplease exec\e[m:    source $HOME/.bashrc"
+echo -e "\e[33mplease exec\e[m:
+
+echo "please exec command: source $HOME/.bashrc"
