fd55e5d963c0daf343f21a95438ef9b8bbdf1cee
No1zy <bgkn1260128@gn.iwasaki.ac.jp>
2017-08-10 12:08:22 -0700

    setup.sh: modified

--- setup.sh
+++ setup.sh
@@ -4,6 +4,5 @@ echo "alias pingg='ping -c 1 google.co.jp'" >> $HOME/.bashrc
 echo "alias curlg='curl google.co.jp'" >> $HOME/.bashrc
 
 echo 'export PATH=$PATH:$HOME/ponpontools/commands/' >> $HOME/.bashrc
-echo -e "\e[33mplease exec\e[m:
+echo -e "\e[33mplease exec\e[m: source $HOME/.bashrc"
 
-echo "please exec command: source $HOME/.bashrc"
